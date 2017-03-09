df737a4af0b5cdb2e1fe4ab31ed06a7264838fec
rickslayer <paulo@actio.net.br>
2017-03-09 13:41:17 -0300

    calcula porcentagem
    09/03/2017

--- facilitadores/js/app.js
+++ facilitadores/js/app.js
@@ -2,9 +2,40 @@ $(function(){
 
     $("#tamanho").on("keypress", function(){
           var tamanho =  $("#tamanho").val();
-
-$("#conta-tamanho").text("Caracteres "+ tamanho.length);
+          $("#conta-tamanho").val("Caracteres "+ tamanho.length);
 
     });
+      $("#tamanho").on("change", function(){
+          var tamanho =  $("#tamanho").val();
+          $("#conta-tamanho").val("Caracteres "+ tamanho.length);
+
+    });
+
+
+      $("#valor").on("change", function(){
+           $("#resultado").val(calculaPorcentagem());
+
+      });
+       $("#porcentagem").on("change", function(){
+           $("#resultado").val(calculaPorcentagem());
+
+      });
+
+      function calculaPorcentagem()
+      {
+         porcentagem =   $("#porcentagem").val();
+         porcentagem = porcentagem.replace(',' , '.');
+         valor       =   $("#valor").val();
+         valor       = valor.replace(',' , '.');
+          var valorTotal;
+
+          if (valor > 0 && porcentagem > 0)
+            {
+                valorTotal = (valor)  * (1 + (porcentagem/100));
+            }
+            return valorTotal;
+
+
+      }
 
 });
