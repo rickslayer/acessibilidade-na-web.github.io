a366f6cc8515bb81db92c64453952a5e954de294
paulo.almeida <paulo.almeida@erpflex.com.br>
2018-04-03 17:25:25 -0300

    add angular material

--- resume/js/app.js
+++ resume/js/app.js
@@ -1,14 +1,16 @@
-var appAbout = angular.module("appResumeAbout", []);
+var appAbout = angular.module('appResumeAbout', ['ngMaterial', 'ngMessages']);
 
-appAbout.controller("appResumeController", function ($scope){
-	
+appAbout.controller("appResumeController",appResumeController);
+
+function appResumeController($scope)
+{
 	$scope.nome = "Paulo Ricardo";
 	$scope.sobrenome = "Rube de Almeida";
 	$scope.endereco = "120 · Urias Pithon Barreto Street · Arujá · São Paulo · Brazil · (55) 97508-5586";
+	$scope.demo = {
+            showTooltip : false,
+            tooltipDirection : ''
+         };
 	
-	$scope.eventoMouseTwitter = function(resp)
-	{
-		$scope.twitter_url = "@rick2slayer";
-	}
-	
-});
+
+}
