c7f53ea6581c0d0e5cada2621130ebfc4b304fbe
paulo.almeida <paulo.almeida@erpflex.com.br>
2018-04-03 15:06:35 -0300

    alteracoes tooltip

--- resume/js/app.js
+++ resume/js/app.js
@@ -6,4 +6,9 @@ appAbout.controller("appResumeController", function ($scope){
 	$scope.sobrenome = "Rube de Almeida";
 	$scope.endereco = "120 · Urias Pithon Barreto Street · Arujá · São Paulo · Brazil · (55) 97508-5586";
 	
+	$scope.eventoMouseTwitter = function(resp)
+	{
+		$scope.twitter_url = "@rick2slayer";
+	}
+	
 });
