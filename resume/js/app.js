a71b3d426ec30b9ecb91df7e85a5f83f1fa36dce
rickslayer <paulo@actio.net.br>
2018-04-03 23:15:23 -0300

    resolvido tooltip redes

--- resume/js/app.js
+++ resume/js/app.js
@@ -1,4 +1,4 @@
-var appAbout = angular.module('appResumeAbout', ['ngMaterial', 'ngMessages']);
+var appAbout = angular.module('appResumeAbout', ['ngMaterial', 'ngMessages', 'ngAnimate']);
 
 appAbout.controller("appResumeController",appResumeController);
 
@@ -6,11 +6,19 @@ function appResumeController($scope)
 {
 	$scope.nome = "Paulo Ricardo";
 	$scope.sobrenome = "Rube de Almeida";
-	$scope.endereco = "120 · Urias Pithon Barreto Street · Arujá · São Paulo · Brazil · (55) 97508-5586";
-	$scope.demo = {
-            showTooltip : false,
-            tooltipDirection : ''
-         };
+	$scope.endereco = "120 · Urias Pithon Barreto Street · Arujá · São Paulo · Brazil · +55 (11) 97508-5586";
 	
+	
+	$scope.hoverIn = function (redesocial, valor){
+		this.hoverEdit = true;
+		var oBJ = {};
+		oBJ[redesocial] = valor;		
+		$scope.socials = oBJ;
+	};
+	
+	 $scope.hoverOut = function(){
+	        this.hoverEdit = false;
+	    };
 
+	
 }
